359f8bb1f2775cf8f46b5497e6fdd8df8e573657
FatumaT <regretmakingthis@gmail.com>
2024-06-27 13:33:24 -0400

    Created Shooter Command

---
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.shooterConstants;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class ShooterCommand extends SubsystemBase {
+  /** Creates a new ShooterCommand. */
+  public ShooterCommand() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
