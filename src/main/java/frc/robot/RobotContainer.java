c2bee1339935c7dc8e83fbdbb3a1221b76cc882f
Famat <71714557+fatuma-tahalil@users.noreply.github.com>
2024-10-25 16:57:05 -0400

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,63 +5,54 @@
 package frc.robot;
 
 // Automatically from template
-import frc.robot.Constants.OperatorConstants;
-
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.DriveCommand;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
-//import edu.wpi.first.cameraserver.CameraServer;
-//import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.Joystick;
-//import edu.wpi.first.wpilibj2.command.RunCommand;
-//import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
-//import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-//import edu.wpi.first.wpilibj2.command.button.Trigger;
-
-import frc.robot.commands.DriveCommand;
-
-import frc.robot.subsystems.DrivetrainSubsystem;
-
-
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
  */
-public class RobotContainer{
+public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+  public static final DrivetrainSubsystem m_drivetrain = new DrivetrainSubsystem();
+  private final ShooterSubsystem m_ShooterSubsystem = new ShooterSubsystem();
 
-  // Imports an object of the drive train subsystem
-  private final DrivetrainSubsystem drivetrainSubsystem;
-  private final DriveCommand driveCommand;
-  private final ShooterSubsystem shooterSubsystem;
-  public static Joystick driverController;
-  //*  Replace with CommandPS4Controller or CommandJoystick if needed
+  public Joystick driverController = new Joystick(Constants.OperatorConstants.controllerPort);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot.  Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
-    driverController = new Joystick(OperatorConstants.controllerPort);
+    // Configure the button bindings
+    configureButtonBindings();
 
-    drivetrainSubsystem = new DrivetrainSubsystem();
-    driveCommand = new DriveCommand(drivetrainSubsystem);
-    shooterSubsystem = new ShooterSubsystem();
-    // Sets the camera
-    /*UsbCamera camera2UsbCamera = CameraServer.startAutomaticCapture();
-    camera2UsbCamera.setResolution(230, 230); */
-
-    // Configure the trigger bindings
-    configureBindings();
+    // Set default commands on subsystems
+   m_drivetrain.setDefaultCommand(new DriveCommand(m_drivetrain, driverController));
   }
 
-  private void configureBindings() {
-
+  /**
+   * Use this method to define your button->command mappings.  Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
   }
+
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
-
+   * @return the command to run in autonomous
    */
-  public void getAutonomousCommand() {}
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return Commands.none();
+  }
 }
